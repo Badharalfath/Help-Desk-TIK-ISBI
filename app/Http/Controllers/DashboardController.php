7ffe2f41322dc56ffe20454ce12f3bf069775324
Badharalfath <94126104+Badharalfath@users.noreply.github.com>
2024-09-17 16:24:48 +0700

    Menambah Statistik Dashboard dan Redirect dashboard jika belum logout

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,7 +15,7 @@ class DashboardController extends Controller
                 SUM(CASE WHEN progress_status = "unsolved" THEN 1 ELSE 0 END) as unsolved,
                 SUM(CASE WHEN progress_status = "ongoing" THEN 1 ELSE 0 END) as ongoing,
                 SUM(CASE WHEN progress_status = "solved" THEN 1 ELSE 0 END) as solved,
-                SUM(CASE WHEN progress_status IS NULL THEN 1 ELSE 0 END) as pending
+                SUM(CASE WHEN progress_status = "pending" THEN 1 ELSE 0 END) as pending
             ')
             ->first();
 
