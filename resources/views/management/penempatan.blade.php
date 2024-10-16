4fd02190a4f58dd1b8a405fd735c8f3358dbfde4
KrakenMiX <dghaz.xrpl5@gmail.com>
2024-10-16 11:21:48 +0700

    add status exception on penempatan and pengadaan

--- resources/views/management/penempatan.blade.php
+++ resources/views/management/penempatan.blade.php
@@ -31,6 +31,15 @@
         @if (session('success'))
             <div class="bg-green-500 text-white p-4 rounded mb-4">{{ session('success') }}</div>
         @endif
+        @if ($errors->any())
+            <div class="bg-red-500 text-white p-3 rounded mb-4">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
         <table class="min-w-full bg-white shadow-md rounded-lg">
             <thead>
--- resources/views/management/pengadaan.blade.php
+++ resources/views/management/pengadaan.blade.php
@@ -13,6 +13,18 @@
 
         <!-- Tabel Transaksi -->
         <div class="overflow-x-auto">
+        @if (session('success'))
+            <div class="bg-green-500 text-white p-4 rounded mb-4">{{ session('success') }}</div>
+        @endif
+        @if ($errors->any())
+            <div class="bg-red-500 text-white p-3 rounded mb-4">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
             <table class="min-w-full bg-white border border-gray-300">
                 <thead>
                     <tr>
